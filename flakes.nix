c22649c1992ce3c5f0b80acc38fe0498020d95e0
nek <82368087+neksiak@users.noreply.github.com>
2026-04-29 18:57:12 +0200

    Add initial NixOS configuration in flakes.nix

---
+++ flakes.nix
@@ -0,0 +1,30 @@
+{
+  description = "nek's nixos config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+  };
+
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        nixos-hardware.nixosModules.common-cpu-amd
+        nixos-hardware.nixosModules.common-cpu-amd-pstate
+        nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.nek = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
